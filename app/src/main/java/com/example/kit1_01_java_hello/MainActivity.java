3f06ffa840f158bc190fd2a694a62aec6c0a855f
afalinalv <afalinalv@ya.ru>
2018-11-19 15:55:30 +0300

    AS 3.3.0-betta4 Gradle 4.10.1 Androidx Hello World school

---
+++ app/src/main/java/com/example/kit1_01_java_hello/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kit1_01_java_hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
